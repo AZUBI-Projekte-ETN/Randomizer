fdcc4b5a4fc08950976dc3edc3912fa537fe411c
Eyuep42 <169087981+Eyuep42@users.noreply.github.com>
2025-02-09 16:38:06 +0100

    Added Randomize Capibility
    Added Randomize Capibility

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,56 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let vorname = document.getElementById('vorname');
+    let name = document.getElementById('name');
     let randBtn = document.querySelector('.rand-button');
 
-    let fullNames = []
+    let fullNames = [];
 
-    vorname.addEventListener("change", addName);
+
+    name.addEventListener("change", addName);
     randBtn.addEventListener("click", randomizeName);
 
 
 
     function addName(e) {
         fullNames.push(e.target.value);
-        vorname.value = '';
-        clearNames();
+        name.value = '';
         showNames();
     }
 
+    function setNameInDom(name) {
+        document.querySelector('.grid-container').innerHTML += `<div class="grid-item">${name}<div/>`
+    }
+
+    function deleteName(index) {
+        fullNames.splice(index, 1);
+    }
+
+    function clearNames() {
+        document.querySelector('.names').innerHTML = '';
+    }
+
     function showNames() {
+       clearNames();
         for (let i = 0;i<fullNames.length;i++) {
             document.querySelector('.names').innerHTML += `<li>${fullNames[i]}</li>`
         }
     }
 
     function randomizeName() {
-        let randInt = Math.floor(Math.random() * fullNames.length)
-        console.log(randInt)
+        if (fullNames.length == 0) {
+            console.error('Please enter Names')
+        }
+        else {
+            console.log(fullNames.length)
+            let randInt = Math.floor(Math.random() * fullNames.length);
+            setNameInDom(fullNames[randInt])
+            deleteName(randInt);
+            showNames();
+        }
+
     }
 
 
-    function clearNames() {
-        document.querySelector('.names').innerHTML = '';
-    }
+
 
 
 })
